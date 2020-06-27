fdf29fee9e975111bbd6fd9935b0fc2c7c61f307
Eric Foster <elitheroyalone@gmail.comgit config --global user.name eric-fostergit config --global user.email elitheroyalone@gmail.com>
2020-06-27 13:11:35 -0500

    updates

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,26 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.age = 0
+        self.list = []
+               
 
     def append(self, item):
-        pass
+        size = len(self.list)
+        capacity = self.capacity
+      
 
-    def get(self):
-        pass
+        if size < capacity:
+            return self.list.append(item)
+        if size >= capacity:
+            if self.age == self.capacity:
+                self.age = 0
+                self.list.insert(self.age, item)
+                self.list.pop(self.age+1)
+                self.age += 1
+            else:
+                self.list.insert(self.age, item)
+                self.list.pop(self.age+1)
+                self.age += 1
+def get(self):
+        print(f"{self.list}")
